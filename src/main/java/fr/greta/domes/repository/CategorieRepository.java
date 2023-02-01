d0af721f3be38d3e687f2e6ea8fdb6dcc031bd36
Git-raymond <raymond.saray@yahoo.com>
2023-02-01 12:03:30 +0100

    suppression annnotation inutiles @repository

--- src/main/java/fr/greta/domes/repository/CategorieRepository.java
+++ src/main/java/fr/greta/domes/repository/CategorieRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import fr.greta.domes.beans.Categorie;
 
-@Repository
+
 public interface CategorieRepository extends CrudRepository<Categorie, String> {
 
 }
