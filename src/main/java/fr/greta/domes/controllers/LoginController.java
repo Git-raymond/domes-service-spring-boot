3aeaf164647be14fa7b67a58edc6d1b5cf400ec6
Git-raymond <raymond.saray@yahoo.com>
2023-03-10 09:40:22 +0100

    maj statut login

--- src/main/java/fr/greta/domes/controllers/LoginController.java
+++ src/main/java/fr/greta/domes/controllers/LoginController.java
@@ -9,6 +9,8 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import fr.greta.domes.entity.Client;
 import fr.greta.domes.service.ClientService;
 
@@ -29,29 +31,26 @@ public class LoginController {
 	public String loginUser(@ModelAttribute("client") Client client, HttpSession session, BindingResult result,
 			ModelMap modelMap) {
 		session.setAttribute("idClient", client.getIdClient());
-		if (clientService.checkLoginClient(client.getEmail(), client.getPassword())
-				&& client.getStatut().equals("actif")) {
-			System.out.println("Succès du login");
-			System.out.println(client.getStatut());
-			return "espaceclient";
-		} else if (clientService.checkLoginClient(client.getEmail(), client.getPassword())
-				&& client.getStatut().equals("suspendu")) {
-			System.out.println("Statut désactivé");
-			System.out.println(client.getStatut());
-			// modelMap.put("erreur", "* Votre comp.te a été suspendu !");
-			result.addError(new FieldError("client", "email", "* Votre compte a été suspendu !"));
-			return "login";
-		} else if (clientService.checkLoginClient(client.getEmail(), client.getPassword())
-				&& client.getStatut().equals("attente")) {
-			System.out.println("Statut désactivé");
-			System.out.println(client.getStatut());
-			// modelMap.put("erreur", "* Votre comp.te a été suspendu !");
-			result.addError(new FieldError("client", "email",
-					"* Veuillez valider le lien de validation envoyé à votre adresse email !"));
-			return "login";
+		if (clientService.checkLoginClient(client.getEmail(), client.getPassword())) {
+			Client temp = clientService.findClientByEmail(client.getEmail());
+			if (temp.getStatut().equals("actif")) {
+				System.out.println("Succès du login");
+				System.out.println(client.getStatut());
+				return "espaceclient";
+			} else if (temp.getStatut().equals("suspendu")) {
+				System.out.println("Statut désactivé");
+				// modelMap.put("erreur", "* Votre comp.te a été suspendu !");
+				result.addError(new FieldError("client", "email", "* Votre compte a été suspendu !"));
+				return "login";
+			} else {
+				System.out.println("Statut en attente de validation");
+				// modelMap.put("erreur", "* Votre comp.te a été suspendu !");
+				result.addError(new FieldError("client", "email",
+						"* Veuillez valider le lien de validation envoyé à votre adresse email !"));
+				return "login";
+			}
 		} else {
 			System.out.println("Echec du login");
-			System.out.println(client.getStatut());
 			// System.out.println(client.getPassword());
 			// modelMap.put("erreur", "* Email et mot de passe invalides !");
 			result.addError(new FieldError("client", "email", "* Email et mot de passe invalides !"));
